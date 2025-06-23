f5b4e5eba92282fe759189bd47fcbf44952968d9
Phil Stahlfeld <phil@stahlfeld.com>
2025-06-22 20:29:47 -0700

    Fix delete request fields

--- src/aipproto/req_res/delete_req_res.py
+++ src/aipproto/req_res/delete_req_res.py
@@ -7,40 +7,20 @@ def from_resource(resource_type: resource.Resource) -> List[render.ReqRes]:
     return [
         render.ReqRes(
             type=f"Delete{pascal}Request",
-            fields=_fields(resource_type),
+            fields=[
+                render.ReqResField(
+                    type="string",
+                    name="name",
+                    comment_lines=[
+                        f"The name of the {pascal} to delete.",
+                    ],
+                    options=[
+                        options.field_behavior("REQUIRED"),
+                        options.resource_reference(
+                            "type", f"{resource_type.namespace().name}/{pascal}"
+                        ),
+                    ],
+                )
+            ],
         ),
     ]
-
-
-def _fields(resource_type: resource.Resource) -> List[render.ReqResField]:
-    pascal = resource_type.format_type("pascal")
-    fields = []
-    if resource_type.parent():
-        fields.append(
-            render.ReqResField(
-                type="string",
-                name="name",
-                comment_lines=[
-                    f"The name of the {pascal} to delete.",
-                ],
-                options=[
-                    options.field_behavior("REQUIRED"),
-                    options.resource_reference(
-                        "type", f"{resource_type.namespace().name}/{pascal}"
-                    ),
-                ],
-            )
-        )
-    fields.extend(
-        [
-            render.ReqResField(
-                type="string",
-                name=f"{resource_type.format_type('snake')}_id",
-            ),
-            render.ReqResField(
-                type=resource_type.format_type("pascal"),
-                name=resource_type.format_type("snake"),
-            ),
-        ]
-    )
-    return fields
